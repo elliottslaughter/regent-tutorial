6d0258192134f53fc58ef85f24b93ed3f3a9130c
Elliott Slaughter <slaughter@cs.stanford.edu>
2018-11-08 11:24:39 -0800

    Add rp2a.sh script.

---
+++ Tasks/rp2a.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --partition=aaiken
+#SBATCH --tasks=1
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=10
+#SBATCH --gres=gpu:4
+#SBATCH --exclusive
+#SBATCH --time=00:05:00
+
+source /home/groups/aaiken/eslaught/tutorial/env.sh
+
+srun regent 2a.rg -hl:prof_logfile prof_%.gz -lg:prof 1 -ll:cpu 4
