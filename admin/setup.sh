a8263a9cc76e401a74e76ea8dfbafb6ff81a46ba
Elliott Slaughter <slaughter@cs.stanford.edu>
2024-08-01 13:54:21 -0700

    Build a release copy of Legion.

--- admin/setup.sh
+++ admin/setup.sh
@@ -13,11 +13,22 @@ cd "$prefix_dir"
 if [[ ! -e legion ]]; then
     git clone git@gitlab.com:StanfordLegion/legion.git
 fi
+if [[ ! -e legion_relase ]]; then
+    cp -r legion legion_release
+fi
+
 pushd legion/language
 # Note: must run this on a compute node
 LAUNCHER="srun -n 1 -N 1 -c 40 -p all --exclusive --pty"
 DEBUG=1 USE_GASNET=0 USE_CUDA=0 $LAUNCHER ./scripts/setup_env.py --cmake -j20
 popd
 
+pushd legion_release/language
+# Note: must run this on a compute node
+LAUNCHER="srun -n 1 -N 1 -c 40 -p all --exclusive --pty"
+DEBUG=0 USE_GASNET=0 USE_CUDA=0 $LAUNCHER ./scripts/setup_env.py --cmake -j20
+popd
+
 mkdir -p bin
 ln -sf ../legion/language/regent.py bin/regent
+ln -sf ../legion_release/language/regent.py bin/regent_release
