16a9b7351424736c5b9f85539ef79d7d456028c0
Elliott Slaughter <slaughter@cs.stanford.edu>
2024-08-01 13:31:27 -0700

    Add admin script for build.

---
+++ admin/setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+root_dir="$(dirname "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
+prefix_dir="$1"
+
+source "$root_dir/env.sh"
+
+mkdir -p "$prefix_dir"
+cd "$prefix_dir"
+
+if [[ ! -e legion ]]; then
+    git clone git@gitlab.com:StanfordLegion/legion.git
+fi
+pushd legion/language
+# Note: must run this on a compute node
+LAUNCHER="srun -n 1 -N 1 -c 40 -p all --exclusive --pty"
+DEBUG=1 USE_GASNET=0 USE_CUDA=0 $LAUNCHER ./scripts/setup_env.py -j20
+popd
+
+mkdir -p bin
+ln -sf legion/language/regent.py bin/regent
